ba352f218eddf1dc78c9255c5b856b83d4402568
lukasz <lukasz.ciswicki@gmail.com>
2024-11-23 00:15:31 +0100

    comment changes

--- lab3/src/algo.hpp
+++ lab3/src/algo.hpp
@@ -6,20 +6,45 @@
 
 class Graph{
     public:
+        /**
+         * Constructs a graph represented by an adjacency list
+         * by parsing a text file
+         */
         Graph(std::string input_path);
+
+        /**
+         * Temp debug constructor
+         */
         Graph(size_t V);
+        
+        size_t V; /* Number of vertices */
+        size_t E; /* Nmber of edges */
+        int max_weight; /* Highest weight of all edges */
 
-        size_t V; // Number of vertices
-        size_t E; // Number of edges
-        int max_weight; // Highest weight of all edges
-
-        // Adjacency list representing a graph.
-        // Edges and weights are stored in form of a pair (vertex, weight)
+        /**
+         * Adjacency list representing a graph.
+         * Edges and weights are stored in form of a pair (vertex, weight)
+         */ 
         std::vector<std::vector<std::pair<size_t, int>>> adjacency_list;
 
+        /**
+         * Ads an edge to the adjacency list
+         * with a weight
+         */
         void add_edge(size_t a, size_t b, int weight);
 
+        /**
+         * Neatly prints the adjacency list
+         */
         void print_graph();
+
+        /**
+         * Parses a text file to create a list of nodes
+         * that will be sources in the ss variant of pahfinding
+         */
+        std::vector<size_t> ss_from_file(std::string ss_path);
+
+
 };
 
-#endif // SRC_ALGO_INCLUDED
+#endif /* SRC_ALGO_INCLUDED */ 
